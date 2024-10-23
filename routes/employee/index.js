5adffe7a28ec8d3368bd21411b290dbea64bd223
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-23 16:36:10 -0700

    initial set up

--- routes/employee/index.js
+++ routes/employee/index.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import * as db from '../../db/index.js';
 
 const router = express.Router();
 
-router.get('/employee', (req, res) => {
-  res.render('employee/index', { title: 'Employee' });
+router.get('/employees', async (req, res, next) => {
+  const query = req.query.q || '';
+  const page = +req.query.page || 1;
+  const pageSize = +req.query.pageSize || 24;
+  const msg = req.query.msg || null;
+
+  try {
+    const total = await db.getEmployeeCount(query);
+    const employees = await db.getEmployeeByName(query, page, pageSize);
+    res.render('./pages/index', {
+      employees,
+      query,
+      msg,
+      currentPage: page,
+      lastPage: Math.ceil(total / pageSize),
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export { router as employeeRouter };
