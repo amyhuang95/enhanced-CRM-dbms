74e23b2c4a579051ce1938cbcbab756297f9b6d3
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-23 16:33:10 -0700

    first complete draft

---
+++ db/employee/get_employee_count.js
@@ -0,0 +1,35 @@
+import { getDBConnection } from '../db_connector.js';
+
+/**
+ * Function to get the count of employees by name from the database
+ * @param {string} query the name query to search for employees.
+ * @returns {Promise<number>} a promise that resolves to the number of employees.
+ */
+export async function getEmployeeCount(query) {
+  console.log('get employee count', query);
+  const db = await getDBConnection();
+
+  const sql = `
+    SELECT COUNT(*) AS count
+    FROM employee
+    WHERE first_name LIKE @query
+      OR last_name LIKE @query;`;
+
+  const params = {
+    '@query': query + '%',
+  };
+
+  try {
+    const stmt = await db.prepare(sql);
+    const result = await stmt.get(params).count;
+    await stmt.finalize();
+    return result;
+  } catch (error) {
+    console.error('Error fetching employee count:', error);
+    throw error;
+  } finally {
+    await db.close();
+  }
+}
+
+export default getEmployeeCount;
