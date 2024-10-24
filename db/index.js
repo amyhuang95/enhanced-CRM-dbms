4ae3607bb911ebdd4da11967b707ed184eb7ab8a
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-23 22:25:31 -0700

    add update employee info feature

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 export * from './employee/get_employee_by_name.js';
 export * from './employee/get_employee_count.js';
 export * from './employee/get_employee_by_id.js';
+export * from './employee/update_employee_by_id.js';
 export * from './customer/get_customer_by_owner_id.js';
--- routes/employee/update.js
+++ routes/employee/update.js
@@ -1,11 +1,28 @@
 import express from 'express';
+import * as db from '../../db/index.js';
 
 const router = express.Router();
 
-// router to update an employee
-router.put('/employee/:id', (req, res) => {
-  const employeeId = req.params.id;
-  res.render('employee/update', { title: 'Update Employee', employeeId });
+/**
+ * POST to update an employee by id
+ */
+router.post('/employees/:employee_id/edit', async (req, res, next) => {
+  const employee_id = req.params.employee_id;
+  const emp = req.body;
+  console.log(emp);
+
+  try {
+    const updateResult = await db.updateEmployeeById(employee_id, emp);
+    console.log('update', updateResult);
+
+    if (updateResult && updateResult.changes === 1) {
+      res.redirect('/employees/?msg=Updated');
+    } else {
+      res.redirect('/employees/?msg=Error Updating');
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 export { router as updateEmployeeRouter };
