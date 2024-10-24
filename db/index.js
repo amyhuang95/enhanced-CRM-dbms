2883a3b101ad48a0e1d8f24015b55050eb5bcd75
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-24 10:49:10 -0700

    set up add new employee

--- db/index.js
+++ db/index.js
@@ -3,5 +3,6 @@ export * from './employee/getEmployeeCount.js';
 export * from './employee/getEmployeeById.js';
 export * from './employee/updateEmployeeById.js';
 export * from './employee/deleteEmployeeById.js';
+export * from './employee/addEmployee.js';
 
 export * from './customer/getCustomerByOwnerId.js';
--- routes/employee/add.js
+++ routes/employee/add.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import * as db from '../../db/index.js';
 
 const router = express.Router();
 
-// router to add a new employee
-router.get('/employee/add', (req, res) => {
-  res.render('employee/add', { title: 'Employee' });
+/**
+ * POST request to add an employee
+ */
+router.post('/addEmployee', async (req, res, next) => {
+  const emp = req.body;
+
+  try {
+    const addEmp = await db.addEmployee(emp);
+
+    console.log('Added', addEmp);
+    res.redirect('/employees/?msg=Employee Added');
+  } catch (err) {
+    console.log('Error adding employee', err);
+    next(err);
+  }
 });
 
 export { router as addEmployeeRouter };
